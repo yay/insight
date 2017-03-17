e9ac69b6d8645b73fa4a5279de76d3468fdc534c
Vitaly Kravchenko <vitalyx@gmail.com>
2017-03-18 00:32:31 +0100

    Populate daily quotes and create an index afterwards.

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -5,17 +5,17 @@ package main
 // https://github.com/Kotlin/kotlinx.coroutines
 
 import org.apache.commons.csv.CSVFormat
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.transactions.transaction
+import org.apache.commons.csv.CSVParser
 import org.joda.time.DateTime
 import org.skife.jdbi.v2.DBI
+import org.skife.jdbi.v2.Handle
 import style.Styles
 import tornadofx.App
 import tornadofx.importStylesheet
 import view.SymbolTableView
 import java.io.File
 import java.math.BigDecimal
+import java.sql.Timestamp
 import java.text.SimpleDateFormat
 
 class InsightApp : App(SymbolTableView::class) {
@@ -26,7 +26,7 @@ class InsightApp : App(SymbolTableView::class) {
 
 }
 
-fun dailyQuotes_fromDiskToDb(db: DBI) {
+fun csvDailyQuotesToDb(db: DBI) {
 
     // TODO: https://www.google.nl/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#newwindow=1&q=ERROR:+numeric+field+overflow&*
 
@@ -37,78 +37,102 @@ fun dailyQuotes_fromDiskToDb(db: DBI) {
     )
     val basePath = "${AppSettings.paths.storage}/data/24-02-2017/"
 
-//    transaction {  // will create a table "dailyquotes"
-//        create(DailyQuotes)
-//    }
+    fun isGoodDecimal(d: BigDecimal): Boolean = d.precision() <= 8 && d.scale() <= 6
 
-    fun isGoodDecimal(d: BigDecimal): Boolean = d.precision() <= 14 && d.scale() <= 6
+    fun writeRecords(handle: Handle, records: CSVParser,
+                     market: String, symbol: String, dateFormat: SimpleDateFormat): Boolean {
+        records.forEach {
+            val rec = it
+            val date = DateTime(dateFormat.parse(rec.get(YahooDataColumns.date)))
+            val open = BigDecimal(rec.get(YahooDataColumns.open))
+            val high = BigDecimal(rec.get(YahooDataColumns.high))
+            val low = BigDecimal(rec.get(YahooDataColumns.low))
+            val close = BigDecimal(rec.get(YahooDataColumns.close))
+            val adjClose = BigDecimal(rec.get(YahooDataColumns.adjClose))
+            val volume = rec.get(YahooDataColumns.volume).toLong()
 
-    transaction {
+            if (isGoodDecimal(adjClose) &&
+                isGoodDecimal(open) &&
+                isGoodDecimal(high) &&
+                isGoodDecimal(low) &&
+                isGoodDecimal(close) &&
+                symbol.length <= 6 &&
+                market.length <= 4) {
+
+                try {
+                    handle.createStatement("insert into dailyquotes (quote_date, symbol, market, \"open\", high, low, \"close\", adj_close, volume) values (:quote_date, :symbol, :market, :open, :high, :low, :close, :adj_close, :volume)")
+                            .bind("quote_date", Timestamp(date.millis))
+                            .bind("symbol", symbol)
+                            .bind("market", market)
+                            .bind("open", open)
+                            .bind("high", high)
+                            .bind("low", low)
+                            .bind("close", close)
+                            .bind("adj_close", adjClose)
+                            .bind("volume", volume)
+                            .execute()
+                } catch (e: Exception) {
+                    println("Exception on ($date, $symbol, $market): $e")
+                    println("$symbol data will not be added to the database.")
+                    handle.execute("rollback")
+                    return false
+                }
+
+            }
+
+        }
+
+        return true
+    }
+
+    db.useHandle {
 
         // IMPORTANT:
         // https://www.postgresql.org/docs/9.1/static/populate.html
 
-        for ((exchange, _market) in exchangeToMarketMap) {
+        val handle = it
+        var index = 0
+
+        for ((exchange, market) in exchangeToMarketMap) {
             val dateFormat = SimpleDateFormat("yyyy-MM-dd")
             val path = basePath + exchange
             val walker = File(path).walk().maxDepth(1)
 
-            for (file in walker) {
-                val _symbol = file.nameWithoutExtension.trim()
-                if (_symbol != exchange) {
-//                println("$value:${file.nameWithoutExtension.trim()}")
+            loop@ for (file in walker) {
+                val symbol = file.nameWithoutExtension.trim()
+                if (symbol != exchange) {
+                    println("${index++} - $market:$symbol")
                     val records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(file.reader())
 
-                    records.forEach {
-                        val rec = it
-                        val date = DateTime(dateFormat.parse(rec.get(YahooDataColumns.date)))
-                        val _open = BigDecimal(rec.get(YahooDataColumns.open))
-                        val _high = BigDecimal(rec.get(YahooDataColumns.high))
-                        val _low = BigDecimal(rec.get(YahooDataColumns.low))
-                        val _close = BigDecimal(rec.get(YahooDataColumns.close))
-                        val _adjClose = BigDecimal(rec.get(YahooDataColumns.adjClose))
-                        val _volume = rec.get(YahooDataColumns.volume).toLong()
+                    handle.execute("begin")
 
-                        if (isGoodDecimal(_adjClose) &&
-                            isGoodDecimal(_open) &&
-                            isGoodDecimal(_high) &&
-                            isGoodDecimal(_low) &&
-                            isGoodDecimal(_close) &&
-                            _symbol.length <= 6 &&
-                            _market.length <= 4) {
-
-                            try {
-                                DailyQuotes.insert {
-                                    it[quoteDate] = date
-                                    it[symbol] = _symbol
-                                    it[market] = _market
-                                    it[open] = _open
-                                    it[high] = _high
-                                    it[low] = _low
-                                    it[close] = _close
-                                    it[adjClose] = _adjClose
-                                    it[volume] = _volume
-                                }
-                            } catch (e: Exception) {
-                                println("Exception on ($date, $_symbol, $_market): $e")
-                            }
-
-                        }
+                    if (writeRecords(handle, records, market, symbol, dateFormat)) {
+                        handle.execute("commit")
+                    } else {
+                        handle.execute("rollback")
                     }
                 }
             }
         }
+
+    }
+}
+
+fun runDbMigration(db: DBI) {
+    val runner = MigrationRunner(db)
+}
+
+fun createTableIndex(db: DBI) {
+    db.useHandle {
+        it.execute("begin")
+        it.execute("alter table dailyquotes add primary key (quote_date, market, symbol)")
+        it.execute("commit")
     }
 }
 
 fun main(args: Array<String>) {
+
     val db = DBI("jdbc:postgresql://localhost:5432/insight")
-    val runner = MigrationRunner(db)
-
-    dailyQuotes_fromDiskToDb(db)
-
-
-//    println(BigDecimal("234.345678"))
 
 //    println(GoogleIntradayData("AAPL").execute().data())
 
